defeafb72dd944d8e5ee6a89ba3300d81161282d
Sunny8747 <tjsdn2560@naver.com>
2020-06-17 23:53:19 +0900

    programmers/hash : lv2_phone done

--- programmers/hash/hash_lv2_phone.cpp
+++ programmers/hash/hash_lv2_phone.cpp
@@ -16,6 +16,20 @@ using namespace std;
 bool solution(vector<string> phone_book)
 {
     bool answer = true;
+    sort(phone_book.begin(), phone_book.end());
+    for (int i = 0; i < phone_book.size(); i++) {
+        //last elem
+        if (i == phone_book.size() - 1) {
+        }
+        else {
+            if (phone_book[i].length() <= phone_book[i + 1].size()) {
+                string temp = phone_book[i + 1].substr(0, phone_book[i].length());
+                if (temp == phone_book[i]) {
+                    return false;
+                }
+            }
+        }
+    }
     return answer;
 }
 
